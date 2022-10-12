da176f12f1b8d7ffd9f6f977b6a5666e1801dab8
DanielB <bradtmoeller@gmail.com>
2022-10-13 00:02:33 +0200

    Create asci_value.py
    add a python printer for ascii value

---
+++ Python/asci_value.py
@@ -0,0 +1,9 @@
+# Python program to print
+# ASCII Value of Character
+ 
+print(" Enter a Character: ")
+
+c_val = input()
+ 
+# print the ASCII value  character 
+print("The ASCII value of '" + c_val + "' is", ord(c_val))
